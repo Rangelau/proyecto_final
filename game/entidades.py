4b0c63999631f1727128a481fa5c784091d46829
rangelau <lau_rangel24@hotmail.com>
2022-10-26 22:54:27 -0500

    Preparacion Class Enemigo

--- game/entidades.py
+++ game/entidades.py
@@ -1,4 +1,7 @@
+
 import pygame as pg
+import random 
+
 
 Ancho = 800
 Alto = 600
@@ -17,7 +20,7 @@ class Jugador(pg.sprite.Sprite):
         self.image = pg.image.load("game/imagenes/nave.png").convert()
         self.image.set_colorkey(negro)
         self.rect = self.image.get_rect()
-        self.rect.centerx = Ancho // 2
+        self.rect.centerx = Ancho // 2-300
         self.rect.centery = Alto//2
         self.w=100
         self.h=56
@@ -26,7 +29,8 @@ class Jugador(pg.sprite.Sprite):
         self.vy=0
 
     def update(self):
-        self.speed_x = 0
+        self.vx = 0
+        self.vy=0
         keystate = pg.key.get_pressed()
         if keystate[pg.K_DOWN]:
             self.vy = 5
@@ -39,14 +43,42 @@ class Jugador(pg.sprite.Sprite):
             self.rect.bottom = Alto
         if self.rect.top< 0:
             self.rect.top = 0
+
+class Enemigos(pg.sprite.Sprite):
+    def __init__(self,w=40,h=40):
+         super().__init__()
+         self.w=w
+         self.h=h
+         self.image = pg.image.load("game/imagenes/a6.png").convert()
+         self.image.set_colorkey(negro)
+        
+         self.rect=self.image.get_rect()
+         self.rect.centerx= random.randrange(Ancho+self.rect.left)
+         self.rect.centery= random.randrange(100,500)
+         self.vx=random.randrange(1,10)
+         
+         
+    def update(self):
+        self.rect.centerx -= self.vx
+        
+        
         
         
 
 lista_objetos = pg.sprite.Group()
 
+#voy a crear variable que me permitra almacenar el grupo de  enemigos
+
+lista_enemigos= pg.sprite.Group()
+
 
 jugador = Jugador()
 lista_objetos.add(jugador)
+
+for i in range (8):
+    enemigo=Enemigos()
+    lista_objetos.add(enemigo)
+    lista_enemigos.add(enemigo)
 
 
 
